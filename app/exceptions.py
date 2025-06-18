2eb37ff0c8dba8b2da985f08d72c279e9403c339
Arman <justarman572@ya.ru>
2025-06-18 20:03:27 +0400

    add comment describing details

--- app/exceptions.py
+++ app/exceptions.py
@@ -3,10 +3,12 @@ from fastapi import HTTPException, status
 
 class BookingException(HTTPException):
     status_code = 500
-    detail = ""
+    detail = "" # Можно не указывать комментарий, потому что для статуса 500 
+                # по умолчанию отображается комментарий "Internal server error"
     
     def __init__(self):
         super().__init__(status_code=self.status_code, detail=self.detail)
+
 
 class UserAlreadyExistsException(BookingException):
     status_code=status.HTTP_409_CONFLICT
