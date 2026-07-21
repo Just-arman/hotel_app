2b7b32274d8f8445c068316f0ce2337735d027eb
Arman <justarman572@ya.ru>
2026-07-21 22:05:23 +0300

    feat: add roles model

--- app/users/models.py
+++ app/users/models.py
@@ -1,4 +1,5 @@
 from typing import TYPE_CHECKING
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, mapped_column, Mapped
 from app.database import Base, str_uniq
 
@@ -17,9 +18,29 @@ class Users(Base):
     phone_number: Mapped[str_uniq]
     email: Mapped[str_uniq]
     hashed_password: Mapped[str]
-    role: Mapped[str] = mapped_column(default="user", server_default="user")
+    role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"), default=1, server_default="1")
 
+    role: Mapped["Roles"] = relationship(back_populates="users", lazy="selectin")
     bookings: Mapped[list["Bookings"]] = relationship(back_populates="user")
 
     def __str__(self):
         return f"Пользователь {self.email}"
+    
+
+class Roles(Base):
+    __tablename__ = "roles"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str_uniq]
+
+    users: Mapped[list["Users"]] = relationship(back_populates="role")
+    
+    # def __repr__(self):
+    #     return f"Roles(name={self.name!r})"
+
+    @property
+    def users_list(self):
+        users = [f"{user.first_name} {user.last_name}" for user in self.users]
+        if len(users) > 5:
+            return ", ".join(users[:5]) + f" и ещё {len(users) - 5} польз."
+        return ", ".join(users)
